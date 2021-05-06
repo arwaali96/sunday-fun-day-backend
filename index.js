e3cf89b44c37791bfb5ec45e26c349476ddb5d5a
Arwa Ali <aaa297@cornell.edu>
2021-05-06 17:31:41 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://aaa297:410AAAaaa@cluster0.kfkyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.port || 5000;
+
+// Creates promise
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true} )
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+    .catch((error) => consolge.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
+// mongodb.com/cloud/atlas
