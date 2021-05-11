45af6e801404694d246bcf9df733838a032e3715
Arwa Ali <aaa297@cornell.edu>
2021-05-11 18:43:38 -0400

    added updatePost to backend database

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,5 @@
+// import { Mongoose } from 'mongoose';''
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
@@ -22,4 +24,15 @@ export const createPost  = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with this id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+    res.json(updatePost);
 }
