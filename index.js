0133d60930fa5ea7cbff8a9d4cf5848f16373564
Arwa Ali <aaa297@cornell.edu>
2021-05-14 20:52:27 -0400

    finalized for deployment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -6,6 +7,7 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
@@ -13,11 +15,15 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://aaa297:410AAAaaa@cluster0.kfkyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.get('/', (res, req) => {
+    res.send('Hello to Activities API')
+});
+
+// const CONNECTION_URL = "mongodb+srv://aaa297:410AAAaaa@cluster0.kfkyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.port || 5000;
 
 // Creates promise
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true} )
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true} )
     .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
     .catch((error) => consolge.log(error.message));
 
